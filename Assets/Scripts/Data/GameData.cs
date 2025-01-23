1ac7cb6b2d1de7d00fdd13e9b723b269e2b7fb24
Velikazov Ruslan <coperonplay3@gmail.com>
2025-01-23 11:09:27 +0300

    Create inventory data

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,15 @@
+public class GameData
+{
+    public int ammoPistol;
+    public int ammoRifle;
+
+    public int kitAmount;
+    
+    public GameData()
+    {
+        ammoPistol = 140;
+        ammoRifle = 250;
+
+        kitAmount = 0;
+    }
+}
---
+++ Assets/Scripts/Data/InventoryData.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class InventoryData : MonoBehaviour
+{
+    public static InventoryData Instance;
+
+    private const string SaveKey = "MainSaveInventory";
+
+    private int _ammoPistol;
+    private int _ammoRifle;
+    private int _kitAmount;
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        Load();
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus)
+        {
+            Save();
+        }
+    }
+
+    private void OnDisable()
+    {
+        Save();
+    }
+
+    private void Load()
+    {
+        var data = SaveManager.Load<GameData>(SaveKey);
+
+        _ammoPistol = data.ammoPistol;
+        _ammoRifle = data.ammoRifle;
+        _kitAmount = data.kitAmount;
+    }
+
+    private void Save()
+    {
+        SaveManager.Save(SaveKey,GetSaveSnapshot());
+        PlayerPrefs.Save();
+    }
+
+    private GameData GetSaveSnapshot()
+    {
+        var data = new GameData()
+        {
+            ammoPistol = _ammoPistol,
+            ammoRifle = _ammoRifle,
+            kitAmount = _kitAmount
+        };
+
+        return data;
+    }
+}
